5db5fc17c07770ae29abf6a0e1dd3e61529ea14a
Nui <letrungdung1113@gmail.com>
2022-12-26 16:12:13 +0700

    tao database migrate

---
+++ database/migrations/2022_12_26_083032_create_works_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('status');
+            $table->enum('level',['easy','medium','hard'])->default('easy');
+            $table->integer('rating');
+            $table->timestamp('time_start');
+            $table->time('time_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+};
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Limitless - Responsive Web Application Kit by Eugene Kopyov</title>
-
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
     <link href="/global_assets/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
